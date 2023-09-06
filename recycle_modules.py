60cafa9e44b205810411d9ceb31f76e92590b0ab
Yuchen Zhang <zhangyuchen000@outlook.com>
2023-09-06 16:28:58 -0500

    Add a 1d2d recycling module.

--- recycle_modules.py
+++ recycle_modules.py
@@ -262,3 +262,90 @@ class NetRcyc2d(Net):
             return cur, output1d
         else:
             return cur
+
+
+class NetRcyc1d2d(Net):
+    def __init__(self, num_1d=None):
+        """
+        Orca 1Mb model with recycling mechanism
+        """
+        super(NetRcyc1d2d, self).__init__(num_1d=num_1d)
+        self.mconv = nn.Conv2d(1, 128, kernel_size=(1, 1), padding=0)
+        self.mEncoder = MultiplicativeEncoder(dim=128, n_mats=1, length=250)
+        self.upSample = nn.Upsample(scale_factor=250)
+        
+    def forward(self, x, mat=None):
+        """Forward propagation of a batch."""
+    
+        def run0(x, mat, dummy):
+            lout1 = self.lconv1(x)
+            out1 = self.conv1(lout1)
+            lout2 = self.lconv2(out1 + lout1)
+            out2 = self.conv2(lout2)
+            lout3 = self.lconv3(out2 + lout2)
+            if mat is not None:
+                mat1d = self.mEncoder(mat)
+                mat1d = self.upSample(mat1d)
+                out3 = self.conv3(lout3 + mat1d)
+            else:
+                out3 = self.conv3(lout3)
+            lout4 = self.lconv4(out3 + lout3)
+            out4 = self.conv4(lout4)
+            lout5 = self.lconv5(out4 + lout4)
+            out5 = self.conv5(lout5)
+            lout6 = self.lconv6(out5 + lout5)
+            out6 = self.conv6(lout6)
+            lout7 = self.lconv7(out6 + lout6)
+            out7 = self.conv7(lout7)
+            mat = out7[:, :, :, None] + out7[:, :, None, :]
+            cur = mat
+            if self.num_1d:
+                output1d = self.final_1d(out7)
+                return cur, output1d
+            else:
+                return cur
+
+        dummy = torch.Tensor(1)
+        dummy.requires_grad = True
+        if self.num_1d:
+            cur, output1d = checkpoint(run0, x, mat, dummy)
+        else:
+            cur = checkpoint(run0, x, mat, dummy)
+
+        def run1(cur, mat):
+            first = True
+            if mat is not None:
+                cur = cur + self.mconv(mat)
+            for lm, m in zip(self.lconvtwos[:7], self.convtwos[:7]):
+                if first:
+                    cur = lm(cur)
+
+                    first = False
+                else:
+                    cur = lm(cur) + cur
+                cur = m(cur) + cur
+            return cur
+
+        def run2(cur):
+            for lm, m in zip(self.lconvtwos[7:13], self.convtwos[7:13]):
+                cur = lm(cur) + cur
+                cur = m(cur) + cur
+            return cur
+
+        def run3(cur):
+            for lm, m in zip(self.lconvtwos[13:], self.convtwos[13:]):
+                cur = lm(cur) + cur
+                cur = m(cur) + cur
+
+            cur = self.final(cur)
+            cur = 0.5 * cur + 0.5 * cur.transpose(2, 3)
+            return cur
+
+        cur = checkpoint(run1, cur, mat)
+        cur = checkpoint(run2, cur)
+        cur = checkpoint(run3, cur)
+
+        if self.num_1d:
+            return cur, output1d
+        else:
+            return cur
